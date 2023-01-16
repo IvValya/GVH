174f0d61dc2d1b16b6d9a0fcd250f8739f861618
Valentina Ivanova <iv.valya.vi85@gmail.com>
2023-01-16 17:25:39 +0300

    category create

--- dishes.js
+++ dishes.js
@@ -1,4 +1,4 @@
-class dishes {
+class dishesL {
   errorHandler(res) {
     if (!res.ok) {
       if (res.status === 401 || res.status === 404)
@@ -10,30 +10,62 @@ class dishes {
     return res;
   }
   load() {
-    debugger;
-    fetch("dishes.json"/*, "GET"*/)
+    fetch("dishes.json" /*, "GET"*/)
       .then(this.errorHandler)
       .then((res) => res.json())
       .then((data) => {
-        const dishes = JSON.parse(data);
-        this.renderCategories(dishes);
+        this.renderCategories(data);
       })
       .catch((err) => console.error(err));
     /* const dishes = JSON.parse(dishes.json);
             this.renderCategories(dishes);*/
   }
 
-  renderCategories(dishes) {
-    let quantityDishes = dishes.length;
+  renderCategories(menu) {
+    let { dishes, tops, toppings } = menu;
+    //let {id, name, products} = dishes;
+    let quantityDishes = menu.length;
     let menuMobileNav = document.querySelector(".menu__mobile_nav");
     console.log(quantityDishes);
-    console.log(dishes);
-
+    console.log(menu);
     const fragment = document.createDocumentFragment();
-    const navLi = menuMobileNav.querySelector(".basketTemp");
-    navLi.forEach((item) => {
-      const cardClone = navLi.content.cloneNode(true);
-      console.log(cardClone);
+    const navLi = menuMobileNav.querySelector(".nav__li_temp");
+    const asideMenuUl = document.querySelector(".asideMenu__ul");
+    const asideNavItem = asideMenuUl.querySelector(".asideItemTemp");
+    let currentNav;
+    dishes.forEach((item) => {
+      if (window.innerWidth < 800) {
+        const cardClone = navLi.content.cloneNode(true);
+        console.log(cardClone);
+        cardClone.querySelector(".nav__li").id = item.id;
+        cardClone.querySelector(".nav__label").textContent = item.name;
+        fragment.append(cardClone);
+        currentNav = document.querySelector(".menu__mobile_nav");
+      } else {
+        const cardClone = asideNavItem.content.cloneNode(true);
+        console.log(cardClone);
+        cardClone.querySelector(".asideMenu__input").id = item.id;
+        cardClone.querySelector(".asideMenu__label").for = item.id;
+        currentNav = document.querySelector(".asideMenu__ul");
+        cardClone.querySelector(".asideMenu__label").textContent = item.name;
+        fragment.append(cardClone);
+      }
     });
+    currentNav.innerHTML = "";
+    currentNav.appendChild(fragment);
+    let fragment1 = document.createDocumentFragment();
+    const menuList = document.querySelector(".menu__list");
+    let dishesListTemp = menuList.querySelector(".dishesListTemp");
+
+    dishes.forEach((item) => {
+      let categoryClone = dishesListTemp.content.cloneNode(true);
+      let menuCat = categoryClone.querySelector(".menu__cat");
+      menuCat.id = item.id;
+      menuCat.dataset.name = item.id;
+      categoryClone.querySelector(".menu__h2").textContent = item.name;
+      fragment1.append(categoryClone);
+    });
+    menuList.innerHTML = "";
+    menuList.appendChild(fragment1);
   }
 }
